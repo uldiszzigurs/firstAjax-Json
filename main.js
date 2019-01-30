ac16b47153c17fb587844f23f74a14eafa833e48
uldiszzigurs <uldis.zzigurs@gmail.com>
2019-01-30 16:04:42 +0200

    basic

---
+++ main.js
@@ -0,0 +1,38 @@
+var pageCounter = 1;
+var animalContainer = document.getElementById('animal-info')
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+    var myRequest = new XMLHttpRequest;
+    myRequest.open('GET','https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json',true);
+    myRequest.onload = function() {
+        myData = JSON.parse(myRequest.responseText);
+        renderHTML(myData); 
+            pageCounter++;
+        if (pageCounter>3) {
+            btn.classList.add('hide-me');
+        }
+    }
+    myRequest.send();
+})
+function renderHTML(data) {
+    var htmlString = '';
+    for (i=0; i < data.length; i++) {
+        htmlString += '<p>' + data[i].name + ' is a ' + data[i].species + ' who likes ';
+        for (j = 0; j<data[i].foods.likes.length;j++) {
+            htmlString +=data[i].foods.likes[j]; 
+            if (j+1<data[i].foods.likes.length) {
+                htmlString += ' and ';
+            }
+        }
+        htmlString += ' although dislikes ';
+        for (j = 0; j<data[i].foods.dislikes.length;j++) {
+            htmlString +=data[i].foods.dislikes[j]; 
+            if (j+1<data[i].foods.dislikes.length) {
+                htmlString += ' and ';
+            }
+        }
+        
+    animalContainer.insertAdjacentHTML('beforeend',htmlString);
+    htmlString = '';
+    }
+}
